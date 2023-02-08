cae4e60fabff11d5ab87d1b9dfadfe153b22fa72
Sam Wu <swu@nait.ca>
2023-02-08 14:41:44 -0700

    added WebApp project

---
+++ NhlSolution/NhlWebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NhlWebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        // Define an auto-implemented property for username
+        [BindProperty]
+        public string Username { get; set; }
+        // Define an auto-implemented property for feedback messages
+        public string? InfoMessage { get; private set; }
+
+        public void OnPost()
+        {
+            // Generate a lucky number between 1 and 50 (inclusive)
+            // and send a feedback message with format:
+            // "Hello {username}. You lucky number is {luckyNumber}"
+            var rand = new Random();
+            var randomNumber = rand.Next(1, 51);
+            InfoMessage = $"Hello {Username}. You lucky number is {randomNumber}";
+        }
+        public void OnGet()
+        {
+
+        }
+    }
+}
