97f3d5b53ade6fd2e5d82da5723d046a91810eaf
Johnne Geograpo <59752069+MyLastStack@users.noreply.github.com>
2023-03-09 11:07:47 -0700

    fin

--- RazorPagesDemo/RazorPagesDemo/Pages/ContactMe.cshtml.cs
+++ RazorPagesDemo/RazorPagesDemo/Pages/ContactMe.cshtml.cs
@@ -6,12 +6,33 @@ namespace RazorPagesDemo.Pages
     public class ContactMeModel : PageModel
     {
         [BindProperty]
-        public string ContactName { get; set; }
+        public string? ContactName { get; set; }
         [BindProperty]
-        public string ContactEmail { get; set; }
+        public string? ContactEmail { get; set; }
         [BindProperty]
-        public string ContactComments { get; set; }
+        public string? ContactComments { get; set; }
         [BindProperty]
+        public Boolean SubscribeToMail { get; set; } = true;
+        public string InfoMessage { get; set; }
+        public string ErrorMessage { get; set; }
+        public void OnPostSendMessage()
+        {
+            string subscribetomail = (SubscribeToMail == true) ? "Yes" : "No";
+            InfoMessage = $"Name: {ContactName} <br />" +
+                $"Email: {ContactEmail} <br />" +
+                $"Comments: {ContactComments} <br />" +
+                $"Subscribe to mail: {subscribetomail}";
+        }
+
+        public IActionResult OnPostClear()
+        {
+            ContactName = null;
+            ContactEmail = null;
+            ContactComments = null;
+            SubscribeToMail = false;
+            return RedirectToPage();
+        }
+
         public void OnGet()
         {
         }
